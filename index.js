3d9509e597c15570ab3f8af3b74ed997a51d10c3
sibghat_ullah <116120489+sibghatullah-dev@users.noreply.github.com>
2025-03-20 10:49:25 +0500

    wrote comment to check the workflow

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ app.use("/api/events", eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//writing comment to check the workflow
