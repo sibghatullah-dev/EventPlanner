4072aa85f95ce3b82a71a409c925aad529690db1
sibghat_ullah <116120489+sibghatullah-dev@users.noreply.github.com>
2025-03-20 11:07:32 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongodb://localhost:27017/);
+        await mongoose.connect("mongodb://localhost:27017/");
         console.log("MongoDB Connected");
     } catch (error) {
         console.error("MongoDB Connection Failed", error);
